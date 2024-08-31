c4fcfb2c10092e8af2f4e5a2d34dd6f239d18e7f
Vinh Le <vinhle@Vinhs-MBP.attlocal.net>
2024-08-31 02:06:24 -0400

    first commit

--- firebase.js
+++ firebase.js
@@ -1,35 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
+// components/AnalyticsInit.js
+import { useEffect } from 'react';
+import { getAnalytics, isSupported } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../path/to/your/firebase/config'; // Update this path
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDGWSC72cyiHSN7DDMSBevTUpQaCbrvGmI",
-  authDomain: "inventory-management-87bb7.firebaseapp.com",
-  projectId: "inventory-management-87bb7",
-  storageBucket: "inventory-management-87bb7.appspot.com",
-  messagingSenderId: "779547406493",
-  appId: "1:779547406493:web:b68a76c7e35dcd6778a474",
-  measurementId: "G-54B3XY1VM4"
-};
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
 
-// Initialize Analytics (only in the client-side)
-const initAnalytics = async () => {
-  if (typeof window !== "undefined") {
-    const supported = await isSupported();
-    if (supported) {
-      const analytics = getAnalytics(app);
-      console.log("Firebase Analytics initialized");
-      // Analytics can now be used
-    } else {
-      console.log("Firebase Analytics is not supported in this environment.");
-    }
-  }
+const AnalyticsInit = () => {
+  useEffect(() => {
+    const initAnalytics = async () => {
+      // Check if Firebase Analytics is supported
+      const supported = await isSupported();
+      if (supported) {
+        // Initialize Firebase Analytics
+        getAnalytics(app);
+        console.log("Firebase Analytics initialized");
+      } else {
+        console.log("Firebase Analytics is not supported in this environment.");
+      }
+    };
+
+    // Run the initialization
+    initAnalytics();
+  }, []);
+
+  return null; // No need to render anything
 };
 
-export { firestore, initAnalytics };
+export default AnalyticsInit;
